402c400da59afd0d7aa3d60be49d72c3b01a8013
DavidBartram <david.bartram@gmail.com>
2024-10-03 18:28:09 +0100

    Remove 2nd authed user

--- k8s/main.tf
+++ k8s/main.tf
@@ -2,12 +2,3 @@ module eks-cluster {
     source = "./modules/eks-cluster"
     project_namespace = var.project_namespace
 }
-
-module eks-auth {
-    source = "./modules/eks-auth"
-    account_id = data.aws_caller_identity.current.account_id
-    eks = module.eks-cluster.eks
-    cluster_admin_iam_user = var.cluster_admin_iam_user
-}
-
-data "aws_caller_identity" "current" {}
