11d4dc29486ba9bfe04798a422f3ebdbd73a206b
DavidBartram <david.bartram@gmail.com>
2024-10-04 12:39:32 +0100

    Make Godbound Dice Roller the default app to host

--- k8s/variables.tf
+++ k8s/variables.tf
@@ -1,6 +1,7 @@
 variable "project_namespace" {
   description = "namespace for project, to use for unique resource naming"
   type        = string
+  default = "TestEksProject"
 }
 
 variable "region" {
@@ -15,22 +16,20 @@ variable "profile" {
   default     = "tf-deploy"
 }
 
-variable "cluster_admin_iam_user" {
-  description = "AWS IAM user to be used as the EKS cluster admin"
-  type        = string
-}
-
 variable "app_name" {
   description = "Name for k8s hosted web app"
   type        = string
+  default = "godbound-dice-roller"
 }
 
 variable container_port {
   description = "Port for k8s hosted web app"
   type        = number
+  default = 5000
 }
 
 variable docker_image {
   description = "Docker image for k8s hosted web app"
   type        = string
+  default = "dbartramdockerhub/godbound-dice-roller:latest"
 }
